5089637219b52e76251c1a87de2d6c1e34fc9086
asabla <sparven89@gmail.com>
2017-07-16 01:06:40 +0200

    Added support for measuring time taken for webrequest

---
+++ src/dotnet_analyze/Resources/DiagnosticsHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_analyze.Resources
+{
+    public class DiagnosticsHelper
+    {
+        public static String FormatString(string msg)
+        {
+            var now = DateTime.Now;
+            return $"{now} - {msg}";
+        }
+
+        public void WriteToConsole(string msg)
+        {
+            var now = DateTime.Now;
+            var tmp = $"{now} - {msg}";
+
+            Console.WriteLine(tmp);
+            System.Diagnostics.Debug.WriteLine(tmp);
+        }
+    }
+}
--- src/dotnet_analyze/WebStopWatchAttribute.cs
+++ src/dotnet_analyze/WebStopWatchAttribute.cs
@@ -1,13 +1,56 @@
-﻿using System;
+﻿using dotnet_analyze.Resources;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 
 namespace dotnet_analyze
 {
     public class WebStopWatchAttribute : ActionFilterAttribute
     {
+        private Stopwatch sw = null;
+        private DiagnosticsHelper helper = null;
+
+        public WebStopWatchAttribute()
+        {
+            this.sw = new Stopwatch();
+            this.helper = new DiagnosticsHelper();
+        }
+
+        #region Sync methods
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            this.sw.Start();
+            base.OnActionExecuting(actionContext);
+        }
+
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            this.helper.WriteToConsole($"Time taken to execute {actionExecutedContext.ActionContext.ActionDescriptor.ActionName}: {this.sw.Elapsed}");
+            this.sw.Stop();
+            base.OnActionExecuted(actionExecutedContext);
+        }
+        #endregion
+
+        #region Async methods
+        public override Task OnActionExecutedAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        {
+            this.sw.Start();
+            return base.OnActionExecutedAsync(actionExecutedContext, cancellationToken);
+        }
+
+        public override Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            this.helper.WriteToConsole($"Time taken to execute {actionContext.ActionDescriptor.ActionName}: {this.sw.Elapsed}");
+            this.sw.Stop();
+            this.sw.Reset();
+            return base.OnActionExecutingAsync(actionContext, cancellationToken);
+        }
+        #endregion
     }
 }
