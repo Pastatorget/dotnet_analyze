dc8ecc98526fa1b6446c5dd03b5ffd850eae6869
asabla <sparven89@gmail.com>
2017-10-22 22:48:30 +0200

    Added .net framework example web app
    This application will later on use all added attributes in this
    repository/library

---
+++ src/web-examples/dotnet-framework/Controllers/ExampleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace dotnet_framework.Controllers
+{
+    public class ExampleController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Fetch([FromUri] int sleepTime)
+        {
+
+
+            return Request.CreateResponse(HttpStatusCode.OK, $"Your response took {sleepTime} seconds to complete");
+        }
+    }
+}
